88370f43ab3441513024f08264c9af1a9313103c
oktadhirga <okta.dhirga@gmail.com>
2019-03-31 10:45:17 +0700

    change host param

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "oktawebserverapp.database.windows.net";
+    $user = "oktadhirga";
+    $pass = "Celoteh89";
+    $db = "oktawebapp";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
